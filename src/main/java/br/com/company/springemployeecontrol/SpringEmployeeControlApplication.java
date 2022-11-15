c43ff4f48fad8bba2789bdf030ae1d09231e5a25
Alberto Uehara <alberto.uehara@gmail.com>
2022-11-15 20:53:38 -0300

    First Commit

---
+++ src/main/java/br/com/company/springemployeecontrol/SpringEmployeeControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.company.springemployeecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeeControlApplication.class, args);
+	}
+
+}
